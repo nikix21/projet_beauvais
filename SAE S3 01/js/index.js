9d6a60944841c11fd008297c27a28a7622549dbb
calli77 <107708655+calli77@users.noreply.github.com>
2022-09-12 16:24:15 +0200

    Add files via upload

---
+++ SAE S3 01/js/index.js
@@ -0,0 +1,10 @@
+var onglet = document.getElementsByClassName('onglet')[0];
+
+function cpre(){
+    var pre = document.getElementById("pre");
+    pre.style.color="orange";
+}
+
+function normal(){
+    onglet.style.color="white";
+}
