0176349dad191ee3ea4242df1e0f0986ad2802be
DuboisCorentin <112875903+DuboisCorentin@users.noreply.github.com>
2022-11-02 11:02:11 +0100

    Delete carousel directory

--- carousel/js/main.js
+++
@@ -1,6 +0,0 @@
-let btn = $("#scroll-to-top");
-$(window).scroll(function() {
-    300 < $(window).scrollTop() ? btn.show() : btn.hide()
-}), btn.on("click", function() {
-    window.scrollTo(0, 0)
-});
