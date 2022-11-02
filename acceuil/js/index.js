4e8300c7602c599e0d2818b75469f9a6d8aa6a5a
DuboisCorentin <112875903+DuboisCorentin@users.noreply.github.com>
2022-11-02 14:58:05 +0100

    Add files via upload

--- acceuil/js/index.js
+++ acceuil/js/index.js
@@ -1,52 +1,38 @@
 
-/*********************************navBar *********************************************/
-var item = document.getElementsByClassName("nav-item");
-var lien = document.getElementsByClassName("nav-link");
-var liste = document.getElementsByClassName("dropdown-menu");
-var img = document.getElementsByClassName("slide");
 
 
-function verification(a){
-    for (let i = 0; i < lien.length; i++) {
-        if(a == "Présentation" ){
-            aufocus(0);
-        }
-        if(a == "Actualités" ){
-            aufocus(1);
-        }
-        if(a == "Aménagement" ){
-            aufocus(2);
-        }
-        if(a == "Economie" ){
-            aufocus(3);
-        }
-        if(a == "Habitat" ){
-            aufocus(4);
-        }
-        if(a == "Déchet" ){
-            aufocus(5);
-        }
-        if(a == "Culture" ){
-            aufocus(6);
-        }
+var btn;
+var liens;
+var ouvert;
+var corps;
+
+function init() {
+    ouvert = false;
+    btn = document.getElementById("btnHmbg");
+    liens = document.getElementById("liensNav");
+    corps = document.body;
+
+    btn.addEventListener("click", SwitchNavbar);
+
+}
+
+
+function SwitchNavbar() {
+    if (ouvert == false) {
+        btn.classList.add("open")
+        liens.classList.add("open")
+        corps.style.overflow = "hidden";
+        ouvert = true;
+    }
+    else {
+        btn.classList.remove("open")
+        liens.classList.remove("open")
+        corps.style.overflowY = "scroll";
+        ouvert = false;
     } 
 }
 
-function aufocus(n){
-    item[n].style.backgroundColor="orange";
-    item[n].style.transition = "all 1s";
-    liste[n].style.display= "block";
-    $(".dropdown-link").slideDown("slow");
-    //liste[n].style.transition = "ease 1s";
-}
-
-function pertefocus(){
-    for (let i = 0; i < item.length; i++) {
-        item[i].style.backgroundColor = "#335494";
-        liste[i].style.display = "none";
-    }
-}
-/*********************************Fin navBar *********************************************/
+window.addEventListener("load",init);
 
 /*********************************Caroussel *********************************************/
 
