c50ed0e6407d1d4e95dbc9184219297975c65e20
DuboisCorentin <112875903+DuboisCorentin@users.noreply.github.com>
2022-11-02 11:00:26 +0100

    Add files via upload

--- tourisme/js/index.js
+++ tourisme/js/index.js
@@ -21,3 +21,35 @@ $.ajax({
 		alert("PB avec l'URL");
 	}
 });
+
+var btn;
+var liens;
+var ouvert;
+var corps;
+
+function init() {
+    ouvert = false;
+    btn = document.getElementById("btnHmbg");
+    liens = document.getElementById("liensNav");
+    corps = document.body;
+
+    btn.addEventListener("click", SwitchNavbar);
+
+}
+
+function SwitchNavbar() {
+    if (ouvert == false) {
+        btn.classList.add("open")
+        liens.classList.add("open")
+        corps.style.overflow = "hidden";
+        ouvert = true;
+    }
+    else {
+        btn.classList.remove("open")
+        liens.classList.remove("open")
+        corps.style.overflowY = "scroll";
+        ouvert = false;
+    } 
+}
+
+window.addEventListener("load",init);
